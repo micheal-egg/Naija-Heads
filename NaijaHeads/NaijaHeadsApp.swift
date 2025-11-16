7bfdd9d7ab330031b7ad6c333ec5a16c3c58c256
Micheal <m.egboh005@gmail.com>
2025-11-15 20:14:24 -0600

    Initial Commit

---
+++ NaijaHeads/NaijaHeadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaijaHeadsApp.swift
+//  NaijaHeads
+//
+//  Created by Micheal Egboh  on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NaijaHeadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
